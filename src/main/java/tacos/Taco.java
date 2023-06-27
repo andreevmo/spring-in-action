5eb11fd65b9aedd6333f3c9a5b8284a586ec8d50
andreevmo <maxiandreev1994@gmail.com>
2023-06-27 15:49:47 +0400

    update: Ingredient.java, Taco.java, TacoOrder.java
    change classes for work with database
    add class IngredientRef

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,18 +1,30 @@
 package tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date createdAt = new Date();
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany()
     private List<Ingredient> ingredients;
 }
--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,5 +1,11 @@
 package tacos;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -7,28 +13,35 @@ import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class TacoOrder {
 
-    @NotBlank(message="Delivery name is required")
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date createdAt = new Date();
+    @NotBlank(message = "Delivery name is required")
     private String deliveryName;
-    @NotBlank(message="Street is required")
+    @NotBlank(message = "Street is required")
     private String deliveryStreet;
-    @NotBlank(message="City is required")
+    @NotBlank(message = "City is required")
     private String deliveryCity;
-    @NotBlank(message="State is required")
+    @NotBlank(message = "State is required")
     private String deliveryState;
-    @NotBlank(message="Zip code is required")
+    @NotBlank(message = "Zip code is required")
     private String deliveryZip;
-    @CreditCardNumber(message="Not a valid credit card number")
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="Must be formatted MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
-    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
-
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
